f04c828a8cd95ff383289329ab46044a4cfedc68
Etash Jhanji <etashjhanji@gmail.com>
2026-05-15 12:28:08 -0400

    Update endpoints

--- backend/router/route.go
+++ backend/router/route.go
@@ -5,7 +5,6 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/scottylabs/cmuinsta/backend/handlers"
-	"github.com/scottylabs/cmuinsta/backend/middleware"
 )
 
 func Setup() *gin.Engine {
@@ -13,13 +12,7 @@ func Setup() *gin.Engine {
 
 	// Authentication endpoint
 	r.POST("/api/v1/auth/callback", handlers.AuthCallback)
-
-	api := r.Group("/api/v1")
-	api.Use(middleware.Authenticate())
-	{
-		api.POST("/me", handlers.CreateUser)
-		api.GET("/me", handlers.GetUser)
-	}
+	r.POST("/api/v1/hook", handlers.DMHook)
 
 	return r
 }

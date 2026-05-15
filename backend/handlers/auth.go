b9bb19ad1a5abb52e488ec94bf899ac05f32a0c8
Etash Jhanji <etashjhanji@gmail.com>
2026-05-15 12:28:17 -0400

    Update auth for PKCE flow

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -11,23 +11,25 @@ import (
 
 // AuthCallback handles the exchange of a code from the frontend OIDC flow to
 // a JWT from Keycloak handled privately in the backend. This is in accordance
-// with RFC 4 for authentication and RFC 2.7 POST `/auth/callback`
+// with RFC 4 for authentication and RFC 2 POST `/auth/callback`
 func AuthCallback(c *gin.Context) {
 	var body struct {
-		Code string `json:"code" binding:"required"`
+		Code     string `json:"code" binding:"required"`
+		Verifier string `json:"code_verifier" binding:"required"`
 	}
 	if err := c.ShouldBindJSON(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing code"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing code or verifier"})
+		return
 	}
 
 	resp, err := http.PostForm(
-		os.Getenv("OIDC_ISSUER_URL")+"protocol/openid-connect/token",
+		os.Getenv("OIDC_ISSUER_URL")+"/protocol/openid-connect/token",
 		url.Values{
 			"grant_type":    {"authorization_code"},
 			"client_id":     {os.Getenv("OIDC_CLIENT_ID")},
-			"client_secret": {os.Getenv("OIDC_CLIENT_SECRET")},
 			"redirect_uri":  {os.Getenv("OIDC_REDIRECT_URI")},
 			"code":          {body.Code},
+			"code_verifier": {body.Verifier},
 		},
 	)
 	if err != nil {
@@ -41,19 +43,18 @@ func AuthCallback(c *gin.Context) {
 		IDToken     string `json:"id_token"`
 		ExpiresIn   int    `json:"expires_in"`
 	}
+	if resp.StatusCode != http.StatusOK {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Code exchange failed"})
+		return
+	}
 	if err := json.NewDecoder(resp.Body).Decode(&tokenResponse); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse token response"})
 		return
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Code exchange failed"})
-		return
-	}
-
 	// Return the JWT to the frontend
 	c.JSON(http.StatusOK, gin.H{
-		"token":      tokenResponse.AccessToken,
+		"token":      tokenResponse.IDToken,
 		"expires_in": tokenResponse.ExpiresIn,
 	})
 }
